34d84adf5e2a77f70aac6ca49b74acda1bc5e334
J4N0J4N <jankovic.jan4@gmail.com>
2024-06-20 12:07:26 +0200

    Vyhodnocovanie zapracovane do funkcie

--- bulls&cows.py
+++ bulls&cows.py
@@ -6,17 +6,9 @@ email: jankovic.jan4@gmail.com
 discord: jano_15654 
 """
 
-"""
-Hi there!
------------------------------------------------
-I've generated a random 4 digit number for you.
-Let's play a bulls and cows game.
------------------------------------------------
-Enter a number:
------------------------------------------------
-"""
 import random
 import vstupna_hodnota 
+import time
 
 ciara = "-" * 47
 
@@ -26,7 +18,7 @@ def vypis_pozdrav():
     Jednoduchá funkcia, ktorá po zavolaní vypíše
     úvodný pozdrav užívateľa.
     """
-    return(
+    print(
         f"Hi there!\n{ciara}\n"
         f"I've generated a random 4 digit number for you.\n"
         f"Let's play a bulls and cows game.\n{ciara}\n"
@@ -54,6 +46,84 @@ def vytvor_nahodne_cislo() -> int:
     nahodne_cislo = int(str(prve_cislo) + ''.join(map(str, zvysne_cisla)))
     return nahodne_cislo
 
+#moje_nahodne_cislo = vytvor_nahodne_cislo()
 
-print(vypis_pozdrav())
-print(vstupna_hodnota.zadaj_hodnotu())
+"""def main(fce: callable): #-> None:
+    start = time.perf_counter()
+    fce()
+    stop = time.perf_counter()
+    print(f"Celkem: {stop - start:.2f} sek")"""
+
+def vyhodnot_cislo_uzivatela():
+
+    moje_nahodne_cislo = vytvor_nahodne_cislo()
+    print(moje_nahodne_cislo)
+    
+    guess_count = 1
+
+    while True:
+        moj_pokus_uhadnut = str((vstupna_hodnota.zadaj_hodnotu()))
+        
+        zoznam = []
+        
+        bull_count = 0
+
+        for cisla in moj_pokus_uhadnut:
+            if cisla in str(moje_nahodne_cislo):
+                zoznam.append(cisla)
+
+        for index, (f, u) in enumerate(zip(str(moje_nahodne_cislo), moj_pokus_uhadnut)):
+            if f == u:
+                bull_count += 1
+
+        cow_count = len(zoznam)
+
+        
+        if bull_count == 4:
+            if guess_count == 1:
+                print(f"Correct, you've guessed the right number in {guess_count} guess!"
+                      f"\n{ciara}\nThat's amazing!")
+            elif guess_count in range(2, 4):
+                print(f"Correct, you've guessed the right number in {guess_count} guesses!"
+                      f"\n{ciara}\nThat's amazing!")
+            elif guess_count in range(4, 11):
+                print(f"Correct, you've guessed the right number in {guess_count} guesses!"
+                      f"\n{ciara}\nThat's average!")
+            else:
+                print(f"Correct, you've guessed the right number in {guess_count} guesses!"
+                      f"\n{ciara}\nThat's not so good!")
+            break
+
+        else:
+            if bull_count and cow_count == 1:
+                print(f"{bull_count} bull"
+                      f"\n{cow_count} cow")
+                guess_count += 1
+            
+            elif bull_count == 0 and cow_count == 1:
+                print(f"{bull_count} bulls"
+                      f"\n{cow_count} cow")
+                guess_count += 1
+
+            elif bull_count == 1 and cow_count > 1:
+                print(f"{bull_count} bull"
+                      f"\n{cow_count} cows")
+                guess_count += 1
+
+            else:
+                print(f"{bull_count} bulls"
+                      f"\n{cow_count} cows")
+                guess_count += 1
+
+
+        
+
+
+
+
+
+#print(vytvor_nahodne_cislo())
+vypis_pozdrav()
+#print(vstupna_hodnota.zadaj_hodnotu())
+vyhodnot_cislo_uzivatela()
+#main(vyhodnot_cislo_uzivatela)
